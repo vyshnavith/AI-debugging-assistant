d42b2be6401af96226ae6894d81ed921e50689e9
vyshnavith <69190765+vyshnavith@users.noreply.github.com>
2025-10-19 15:23:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from ai_engine import explain_bug
+from rich.console import console
+from rich.markdown import markdown
 
+console = console()
+def main():
+  console.print("[bold green ]AI debugging Assistant[/bold green]\n")
+  error_log = console.input("[bold yellow]Paste your error log:[/blod yellow]\n")
+
+console.print("\n[bold cyan]Analyzing...[/bold cyan]\n")
+result = explain_bug(error_log)
+console.print(markdown(result))
+
+if __name__=="__main__":
+  main()
